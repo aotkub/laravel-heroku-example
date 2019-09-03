79064220f1b9f2ddefeb86b9c2eb7bfd439c20c7
aotkub <51689245+aotkub@users.noreply.github.com>
2019-09-03 11:05:42 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'eh2XxgO0Pb'),
             'username' => env('DB_USERNAME', 'eh2XxgO0Pb'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'UmakuwXpys'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
