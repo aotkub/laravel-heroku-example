607d354d2b7f39e475bedb02c877d31136713a80
aotkub <51689245+aotkub@users.noreply.github.com>
2019-09-17 11:58:22 +0700

    Create users.php

---
+++ resources/views/users.php
@@ -0,0 +1,11 @@
+<html lang ="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Show View</title>
+  </head>
+  <body>
+         <h1> view Data :</h1>
+         
+  </body>
+ </html>
+  
