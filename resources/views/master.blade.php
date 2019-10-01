95f72e5110ea87680abb85b0901304a1ce1c9688
aotkub <51689245+aotkub@users.noreply.github.com>
2019-10-01 09:42:29 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr"
   <head
     <meta charset="utf-8">
-    <title></title>
+    <title>@yield('title')</title>
   </head>
   <body>
-  
+        @yield('content')
   </body>
+  <footer>@yield('footer')</footer>
 </html
