8c798cef9763b97f0a1b30d35c452b3bbc03ec39
aotkub <51689245+aotkub@users.noreply.github.com>
2019-10-01 09:26:26 +0700

    Update HolloController.php

--- app/Http/Controllers/HolloController.php
+++ app/Http/Controllers/HolloController.php
@@ -6,11 +6,8 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-  function showHello($name){
-    return '<h1>Hello:'.$name.'</h1>';
-  }
-  function show(){
-    return view('users')
+    function show(){
+      return view('users')
       ->with('name','natthaphat')
       ->with('title','Laravel tutorial'):
   }
