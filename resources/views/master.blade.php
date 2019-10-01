1db2c753202a43f617c8226a216e2cef58f6613f
aotkub <51689245+aotkub@users.noreply.github.com>
2019-10-01 10:16:14 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,11 @@
   <head
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    <style media="screen">
+      body{
+        background-color: yellow;
+      }
+    </style>
   </head>
   <body>
         @yield('content')
